1560d9de7c4f478649e0b6d1cc97007519569d5a
PeterTP <0petertp@gmail.com>
2023-10-21 16:19:50 +0100

    Initial commit

---
+++ src/main/kotlin/com/petertp/tutorial/TutorialDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.petertp.tutorial
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object TutorialDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
